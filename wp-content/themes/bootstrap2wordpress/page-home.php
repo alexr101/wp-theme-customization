955590e1fb9e66ce9f2bb5ecf67679a510e86f1b
alexr101 <alexonezero@outlook.com>
2017-10-31 22:03:14 -0400

    add advanced custom fields to course section

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -13,7 +13,7 @@ $opt_in_text          = get_post_meta(10, opt_in_text, true);
 $opt_in_btn_text      = get_post_meta(10, opt_in_btn_text, true);
 
 // Advanced Custom Fields
-
+// Boost Your Income Section 
 $income_img                   = get_field('income_feature_image');
 $income_title                 = get_field('income_title');
 $income_description           = get_field('income_description');
@@ -21,6 +21,12 @@ $income_reason_1_title        = get_field('income_reason_1_title');
 $income_reason_1_description  = get_field('income_reason_1_description');
 $income_reason_2_title        = get_field('income_reason_2_title');
 $income_reason_2_description  = get_field('income_reason_2_description');
+
+// Who Should Take This Course Section 
+
+$take_course_title    = get_field('take_course_title');
+$take_course_img    = get_field('take_course_image');
+$take_course_content  = get_field('take_course_content');
 
 get_header(); ?>
 
@@ -119,33 +125,19 @@ get_header(); ?>
 ================================================== -->
 <section id="who-benefits">
   <div class="container">
-    
+
     <div class="section-header">
-      <img src="<?php bloginfo('stylesheet_directory')?>/assets/img/icon-pad.png" alt="Pad and pencil">
-      <h2>Who Should Take This Course?</h2>
+      <!-- If user uploads image -->
+      <?php if(!empty($take_course_img)): ?>
+        <img src="<?php echo $take_course_img['url'] ?>" alt="<?php echo $take_course_img['alt'] ?>">
+      <?php endif ?>
+
+      <h2><?php echo $take_course_title ?></h2>
     </div><!-- section-header -->
     
     <div class="row">
       <div class="col-sm-8 col-sm-offset-2">
-      
-        <h3>Graphic &amp; Web Designers</h3>
-        <p>Graphic designers are extremely talented, but ask them to code their designs and they'll freeze up! This leaves them with no other choice but to hire a web developer. Any professional graphic designers knows web developers can be expensive.</p>
-        <p>If you&rsquo;re a designer, learning to code your own WordPress websites can change your business entirely! Now, not only are you a great designer, but you're a skillful developer, too! This puts you in a position to <strong>make an extra $1,000 - $5,000 per project.</strong></p>
-
-        <h3>Entrepreneurs</h3>
-        <p>Entrepreneurs have big dreams, and in many cases, shoestring budgets. In order to survive in the cut-throat world of the Startup company, it&rsquo;s a necessity to have a world-class website.  However, world-class websites come with a large price tag.</p>
-        <p>If you can learn how to build a high-quality startup website by yourself, then you&rsquo;ve just saved yourself a lot of cash, <strong>tens of thousands of dollars in many cases.</strong></p>
-
-        <h3>Employees</h3>
-        <p>Any company knows the education &amp; training of their employees is key to a thriving team.</p>
-        <p>Depending on the type of company you work for, if you understand how to code, and can develop CMS driven websites, that gives you <strong>negotiating power for a better position, or a higher salary.</strong></p>
-
-        <h3>Code Hobbyists</h3>
-        <p>It&rsquo;s fun to learn challenging new skills. Code hobbyists can add dynamic websites to their arsenal of tools to play with &mdash; you can even <strong>sell WordPress themes and plugins for cash!</strong> The possibilities are truly endless.</p>
-
-        <h3>People Looking for a New Career</h3>
-        <p>Are you out of work? Looking for a more rewarding job? Desire a career that can allow you to work almost anywhere in the world? Becoming a Web Developer might be the answer for you.</p>
-        <p><strong>Web developers are paid well, anywhere from $33,000 to more than $105,000 per year.</strong> They get to work at amazing companies that are changing the world, or they enjoy the ability to start their own companies, become location-independent and work from home, from coffee shops, in an airplane, on the beach, or wherever they want!</p>
+        <?php echo $take_course_content ?>
         
       </div><!-- end col -->
     </div><!-- row -->
